f6d6da9d89435b6c7597783b4a5d0f2e2a5f6627
Your Name <you@example.com>
2024-11-26 23:32:10 -0300

    Detalles de producto y fabricantes/componentes

--- src/pages/DetalleProductos.jsx
+++ src/pages/DetalleProductos.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export default function DetalleProductos() {
+  const [productos, setProductos] = useState([])
+ 
+  const obtenerProductos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/productos/')
+      const datos = await response.json()
+      setProductos(datos)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    obtenerProductos()
+  }, [])
+
   return (
-    <div>DetalleProductos</div>
-  )
+    <div className="container mx-auto p-6">
+    <h1 className="text-2xl font-bold mb-6">Lista de Productos</h1>
+    <ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      {productos.map((producto) => (
+        <li
+          key={producto.id}
+          className="border p-4 rounded-lg shadow-md bg-white"
+        >
+          <img
+            src={`http://localhost:5173/${producto.pathImg}`}
+            alt={producto.nombre}
+            className="w-full h-48 object-cover rounded-md mb-4"
+          />
+          <h2 className="text-lg font-bold">{producto.nombre}</h2>
+          <p className="text-gray-600">{producto.descripcion}</p>
+        </li>
+      ))}
+    </ul>
+  </div>
+  );
 }
+
--- src/pages/FabricantesYComponentes.jsx
+++ src/pages/FabricantesYComponentes.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 export default function FabricantesYComponentes() {
+  const [fabricantes, setFabricantes] = useState([])
+  const [componentes, setComponentes] = useState([])
+
+  const obtenerFabricantes = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/fabricantes/')
+      const datos = await response.json()
+      setFabricantes(datos)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const obtenerComponentes = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/componentes/')
+      const datos = await response.json()
+      setComponentes(datos)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    obtenerFabricantes()
+    obtenerComponentes()
+  }, [])
+
   return (
-    <div>FabricantesYComponentes</div>
-  )
+    <div>
+      <div className="container mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-6">Lista de Fabricantes</h1>
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {fabricantes.map((fabricante) => (
+            <li
+              key={fabricante.id}
+              className="border p-4 rounded-lg shadow-md bg-white"
+            >
+              <h2 className="text-lg font-bold">{fabricante.nombre}</h2>
+              <p className="text-gray-600">{fabricante.direccion}</p>
+              <p className="text-gray-600">{fabricante.numeroContacto}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="container mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-6">Lista de Componentes</h1>
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {componentes.map((componente) => (
+            <li
+              key={componente.id}
+              className="border p-4 rounded-lg shadow-md bg-white"
+            >
+              <h2 className="text-lg font-bold">{componente.nombre}</h2>
+              <p className="text-gray-600">{componente.descripcion}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+  </div>
+  );
 }
+
